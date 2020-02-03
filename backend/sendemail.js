6749702cecebf391671e8dff60b6065d9b6cd99d
Dhruv <jawalkad@oregonstate.edu>
2020-02-03 14:11:56 -0800

    updated sendgrid api

--- backend/sendemail.js
+++ backend/sendemail.js
@@ -1,9 +1,9 @@
 const sgMail = require("@sendgrid/mail");
-process.env.SENDGRID_API_KEY = "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";
+process.env.SENDGRID_API_KEY = "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";
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
     to:"jawalkad@oregonstate.edu",
-    from:"noreply@dealotd.live",
+    from:"dealotd.live@gmail.com",
     subject:"Todays Deal of the Day",
     html: "Get a free Large Double Chocolate Mocha with whipped cream. Use coupon code: <strong>ILOVECS562</strong>"
 }
